d8b654f1e28bf71af7ed0fc0131dc35bde4d6136
mel dejesus <mel dejesus>
2017-04-02 15:20:40 -0400

    again..

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,121 +1,114 @@
 // 1. default topics
-var topics= ["art", "nature", "music"];
+var topics = ["art", "nature", "music"];
 var topicId;
 var newTopic;
 
 // 2. Create button row
 
 function renderButtons() {
-// delete what was there first
-        $(".buttons").empty();
+  // delete what was there first
+  $(".buttons").empty();
 
-//loop thru array of movies for initial buttons
-       for (var i=0; i<topics.length; i++) {
-        var button=$("<button>").attr("id", i).text(topics[i]).css({"padding": "1%", "font-size": "100%"});
-         $(".buttons").append(button);
+  //loop thru array of movies for initial buttons
+  for (var i = 0; i < topics.length; i++) {
+    var button = $("<button>").attr("id", i).text(topics[i]).css({
+      "padding": "1%",
+      "font-size": "100%"
+    });
+    $(".buttons").append(button);
 
-        }
-        $(".button").css("margin-left", "40%");
-      }; //closes render buttons
-
+  }
+  $(".button").css("margin-left", "40%");
+}; //closes render buttons
 
 /// 3. GRAB IMAGES --- 
 $(".buttons").on("click", function(event) {
-      console.log("button clicked");
-      // topicId = $(this).attr("id");
-      topicId=event.target.id;
-      topics[topicId];
+  console.log("button clicked");
+  // topicId = $(this).attr("id");
+  topicId = event.target.id;
+  topics[topicId];
 
-      // Construct search variable
-      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
-        topics[topicId] + "&api_key=dc6zaTOxFJmzC&limit=10";
+  // Construct search variable
+  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+    topics[topicId] + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-      // Ajax request
-      $.ajax({
-          url: queryURL,
-          method: "GET"
-        })
-        // After data comes back from the request
-        .done(function(response) {
-          // store the data in the results variable
-          var results = response.data;
-          var imageDiv;
-                $("#image-here").empty();
-          // Looping through each result item from gathered data
-              for (var i = 0; i < results.length; i++) {
-            // Creating and storing a div tag
+  // Ajax request
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+    // After data comes back from the request
+    .done(function(response) {
+      // store the data in the results variable
+      var results = response.data;
+      var imageDiv;
+      $("#image-here").empty();
+      // Looping through each result item from gathered data
+      for (var i = 0; i < results.length; i++) {
+        // Creating and storing a div tag
 
-                imageDiv = $("<div>");
+        imageDiv = $("<div>");
 
-            // Creating a paragraph tag with the result item's rating
-                var p = $("<p>").text("Rating: " + results[i].rating);
+        // Creating a paragraph tag with the result item's rating
+        var p = $("<p>").text("Rating: " + results[i].rating);
 
-                // Creating and storing an image tag
-                var image = $("<img>");
-            
-            // Setting the src attribute of the image to a property pulled off the result item
-                image.attr({"src": results[i].images.fixed_height.url, "data-still": results[i].images.fixed_height_still.url, "data-animate": results[i].images.fixed_height.url, "data-state": "animate"}).addClass("gif").css("width", "60%"); 
-            
-            // Appending the paragraph and image tag to the animalDiv
-            imageDiv.append(p);
-            imageDiv.prepend(image).css("float", "left");
+        // Creating and storing an image tag
+        var image = $("<img>");
 
-            // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-            $("#image-here").prepend(imageDiv);
-          } //for loop
-      
-     
+        // Setting the src attribute of the image to a property pulled off the result item
+        image.attr({
+          "src": results[i].images.fixed_height.url,
+          "data-still": results[i].images.fixed_height_still.url,
+          "data-animate": results[i].images.fixed_height.url,
+          "data-state": "animate"
+        }).addClass("gif").css("width", "60%");
 
-$("img").on("click", function() {
-     console.log("add image");
-      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-      var state = $(this).attr("data-state");
-      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-      // Then, set the image's data-state to animate
-      // Else set src to the data-still value
-      if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-      } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-      }
-    }); // close still-animate click event
-          
+        // Appending the paragraph and image tag to the animalDiv
+        imageDiv.append(p);
+        imageDiv.prepend(image).css("float", "left");
 
-            }) //done function??? 
-      }); //click event
-        // I HAD ABOVE TWO AFTER THE DONE-FOR LOOP WITHIN THE FIRST EVENT BUT DIDN'T WORK.  
+        // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+        $("#image-here").prepend(imageDiv);
+      } //for loop
+
+      $("img").on("click", function() {
+        console.log("add image");
+        // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+        var state = $(this).attr("data-state");
+        // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+        // Then, set the image's data-state to animate
+        // Else set src to the data-still value
+        if (state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+        } else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
+        }
+      }); // close still-animate click event
+
+    }) //done function??? 
+}); //click event
+// I HAD ABOVE TWO AFTER THE DONE-FOR LOOP WITHIN THE FIRST EVENT BUT DIDN'T WORK.  
 
 //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-      // This function handles events where one button is clicked
-      $("#add-button").on("click", function(event) {
-        console.log("add button")
-        // event.preventDefault() prevents the form from trying to submit itself.
-        // We're using a form so that the user can hit enter instead of clicking the button if they want
-        event.preventDefault();
+// This function handles events where one button is clicked
+$("#add-button").on("click", function(event) {
+  console.log("add button")
+    // event.preventDefault() prevents the form from trying to submit itself.
+    // We're using a form so that the user can hit enter instead of clicking the button if they want
+  event.preventDefault();
 
-        // This line will grab the text from the input box
-        newTopic = $("#new-button").val().trim();
-        $("#new-button").val("");
-        // The movie from the textbox is then added to our array
-        topics.push(newTopic);
+  // This line will grab the text from the input box
+  newTopic = $("#new-button").val().trim();
+  $("#new-button").val("");
+  // The movie from the textbox is then added to our array
+  topics.push(newTopic);
 
-        // calling renderButtons which handles the processing of our movie array
-        renderButtons();
-      
-});//closes  add-button click event
+  // calling renderButtons which handles the processing of our movie array
+  renderButtons();
 
-      // Calling the renderButtons function at least once to display the initial list of movies
-      renderButtons();
+}); //closes  add-button click event
 
-
- 
-// 4. When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
-
-// 5. Under every gif, display its rating (PG, G, so on). 
-
-//    * This data is provided by the GIPHY API.
-//    * Only once you get images displaying with button presses should you move on to the next step.
-
-// 6. Add a form to your page takes the value from a user input box and adds it into your `topics` array. Then make a function call that takes each topic in the array remakes the buttons on the page.
+// Calling the renderButtons function at least once to display the initial list of movies
+renderButtons();
